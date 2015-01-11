1ee2ae69dcfbd30fffb359c0908e72f9193743ba
NETProjects <support@netprojects.gr>
2015-01-11 14:52:37 +0200

    Application container functions

--- DataInterfaces/Libraries/SharedLib/Applications/IApplicationContainer.cs
+++ DataInterfaces/Libraries/SharedLib/Applications/IApplicationContainer.cs
@@ -16,5 +16,29 @@ namespace SharedLib.Applications
         /// <param name="ignoreId">Profile id to ignore.</param>
         /// <returns>True or false.</returns>
         bool NamedSharedProfileExists<T>(string name, int ignoreId);
+
+        /// <summary>
+        /// Gets container categories.
+        /// </summary>
+        IEnumerable<ICategory> Categories { get; }
+
+        /// <summary>
+        /// Gets container applications.
+        /// </summary>
+        IEnumerable<IApplicationProfile> Applications { get; }
+
+        /// <summary>
+        /// Gets application by id.
+        /// </summary>
+        /// <param name="id">Application id.</param>
+        /// <returns>Application profile, null case no profile found.</returns>
+        IApplicationProfile FindApplication(int id);
+
+        /// <summary>
+        /// Gets category by id.
+        /// </summary>
+        /// <param name="id">Category id.</param>
+        /// <returns>Category, null case no category found.</returns>
+        ICategory FindCategory(int id);
     }
 }
--- DataInterfaces/Libraries/SharedLib/Applications/ICategory.cs
+++ DataInterfaces/Libraries/SharedLib/Applications/ICategory.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Data;
 
 namespace SharedLib.Applications
 {
     public interface ICategory
     {
+        /// <summary>
+        /// Gets category id.
+        /// </summary>
         int ID { get; set; }
+
+        /// <summary>
+        /// Gets category guid.
+        /// </summary>
         Guid Guid { get; set; }
+
+        /// <summary>
+        /// Gets category name.
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Gets parent category id.
+        /// </summary>
         int ParentID { get; set; }
+
+        /// <summary>
+        /// Gets if category children present.
+        /// </summary>
+        bool HasChildren { get; }
+
+        /// <summary>
+        /// Gets if child filter is set.
+        /// </summary>
+        bool HasFilter { get; }
+
+        /// <summary>
+        /// Gets current filter.
+        /// </summary>
+        string Filter { get; }
+
+        /// <summary>
+        /// Gets Hierarchical category view.
+        /// </summary>
+        ListCollectionView HierarchicalView { get; }
+
+        /// <summary>
+        /// Gets children.
+        /// </summary>
+        ObservableCollection<object> Children { get; }
     }
 }
