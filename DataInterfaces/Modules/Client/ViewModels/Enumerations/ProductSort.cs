013ec65d0177bc6687311fbe0c28d85a64f1eada
klapsis-netprojects <klapsis@netprojects.gr>
2021-02-15 17:01:24 +0200

    Task 111: Add sorting for products.

---
+++ DataInterfaces/Modules/Client/ViewModels/Enumerations/ProductSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Client.ViewModels
+{
+    /// <summary>
+    /// Defines client purchase view model product sort type.
+    /// </summary>
+    public enum ProductSort
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        Name = 0,
+
+        /// <summary>
+        /// Date added.
+        /// </summary>
+        DateAdded = 1,
+
+        /// <summary>
+        /// Price.
+        /// </summary>
+        Price = 2,
+
+        /// <summary>
+        /// Points price.
+        /// </summary>
+        PointsPrice = 3,
+    }
+}
