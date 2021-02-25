061f8eff3b939e565d1c3cb69cc1f94385e0112a
klapsis-netprojects <klapsis@netprojects.gr>
2021-02-25 18:16:59 +0200

    Add ProductException.

---
+++ DataInterfaces/Modules/Service/Enumerations/Errors/ProductErrorCode.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerService
+{
+    /// <summary>
+    /// Product error codes.
+    /// </summary>
+    public enum ProductErrorCode
+    {
+        /// <summary>
+        /// Set when we try to perform an action on the wrong type of product.
+        /// </summary>
+        InvalidType,
+    }
+}
---
+++ DataInterfaces/Modules/Service/Exceptions/ProductException.cs
@@ -0,0 +1,22 @@
+﻿using Gizmo.Shared.Exceptions;
+
+namespace ServerService.Exceptions
+{
+    /// <summary>
+    /// Product exception.
+    /// </summary>
+    public class ProductException : ErrorCodeExceptionBase<ProductErrorCode>
+    {
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="errorCode">Error code.</param>
+        public ProductException(ProductErrorCode errorCode) : base(errorCode)
+        {
+        }
+
+        #endregion
+    }
+}
