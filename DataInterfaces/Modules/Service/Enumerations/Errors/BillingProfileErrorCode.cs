3016b7791f663aebbc26a19e11fdf416817c904d
klapsis-netprojects <klapsis@netprojects.gr>
2021-03-03 16:15:39 +0200

    Add BillingProfileException.

---
+++ DataInterfaces/Modules/Service/Enumerations/Errors/BillingProfileErrorCode.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerService
+{
+    /// <summary>
+    /// Billing profile error codes.
+    /// </summary>
+    public enum BillingProfileErrorCode
+    {
+        /// <summary>
+        /// Set when we try to create a rate step to an invalid minute.
+        /// </summary>
+        InvalidMinute = 0,
+
+        /// <summary>
+        /// Set when we try to create a rate with multiple loop steps.
+        /// </summary>
+        MultipleLoops = 1,
+    }
+}
---
+++ DataInterfaces/Modules/Service/Exceptions/BillingProfileException.cs
@@ -0,0 +1,35 @@
+﻿using Gizmo.Shared.Exceptions;
+using System;
+using System.Runtime.Serialization;
+
+namespace ServerService.Exceptions
+{
+    /// <summary>
+    /// Billing profile exception.
+    /// </summary>
+    [Serializable()]
+    [DataContract()]
+    public class BillingProfileException : ErrorCodeExceptionBase<BillingProfileErrorCode>
+    {
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="errorCode">Error code.</param>
+        public BillingProfileException(BillingProfileErrorCode errorCode) : base(errorCode)
+        { }
+
+        /// <summary>
+        /// Serialization constructor.
+        /// </summary>
+        /// <param name="info">Serialization info.</param>
+        /// <param name="context">Serialization context.</param>
+        protected BillingProfileException(SerializationInfo info,
+           StreamingContext context)
+            : base(info, context)
+        { }
+
+        #endregion
+    }
+}
