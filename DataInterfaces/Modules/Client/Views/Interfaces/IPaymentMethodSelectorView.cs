98664a46e064acfdf766976b344b9b6c1115205d
klapsis-netprojects <klapsis@netprojects.gr>
2021-03-22 13:13:28 +0200

    Task 36: Payment method selector.

---
+++ DataInterfaces/Modules/Client/Views/Interfaces/IPaymentMethodSelectorView.cs
@@ -0,0 +1,11 @@
+﻿using SharedLib.Views;
+
+namespace Client.Views
+{
+    /// <summary>
+    /// Payment method selector view interface.
+    /// </summary>
+    public interface IPaymentMethodSelectorView : IView
+    {
+    }
+}
