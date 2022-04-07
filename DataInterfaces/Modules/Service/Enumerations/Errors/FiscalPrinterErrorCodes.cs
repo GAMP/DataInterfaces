79ed074bf139cf2c598ea80792aba4b563566634
Oleg Savelos <olegsavelos@gmail.com>
2022-04-07 20:13:07 +0300

    Added fiscal printer exception.

---
+++ DataInterfaces/Modules/Service/Enumerations/Errors/FiscalPrinterErrorCodes.cs
@@ -0,0 +1,26 @@
+﻿using Localization;
+
+namespace ServerService.Exceptions
+{
+    /// <summary>
+    /// Fiscal printer error codes.
+    /// </summary>
+    public enum FiscalPrinterErrorCodes
+    {
+        /// <summary>
+        /// Fiscal printer is not connected.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_NOT_CONNECTED")]
+        NotConnected=0,
+        /// <summary>
+        /// Shift is expired.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_SHIFT_EXPIRED")]
+        ShiftExpired =1,
+        /// <summary>
+        /// Shift is closed.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_SHIFT_CLOSED")]
+        ShiftClosed =2,
+    }
+}
---
+++ DataInterfaces/Modules/Service/Exceptions/FiscalPrinterException.cs
@@ -0,0 +1,35 @@
+﻿using Gizmo.Shared.Exceptions;
+using System;
+using System.Runtime.Serialization;
+
+namespace ServerService.Exceptions
+{
+    /// <summary>
+    /// Fiscal printer exception exception.
+    /// </summary>
+    [Serializable()]
+    [DataContract()]
+    public class FiscalPrinterException : ErrorCodeExceptionBase<FiscalPrinterErrorCodes>
+    {
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="errorCode">Error code.</param>
+        public FiscalPrinterException(FiscalPrinterErrorCodes errorCode) : base(errorCode)
+        { }
+
+        /// <summary>
+        /// Serialization constructor.
+        /// </summary>
+        /// <param name="info">Serialization info.</param>
+        /// <param name="context">Serialization context.</param>
+        protected FiscalPrinterException(SerializationInfo info,
+           StreamingContext context)
+            : base(info, context)
+        { }
+
+        #endregion
+    }
+}
