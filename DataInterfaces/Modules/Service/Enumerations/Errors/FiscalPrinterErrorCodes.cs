ae8098021d4de58608ebb78a3d72b14e6674700c
Oleg Savelos <olegsavelos@gmail.com>
2022-04-13 18:50:06 +0300

    Added/changed localizations.

--- DataInterfaces/Modules/Service/Enumerations/Errors/FiscalPrinterErrorCodes.cs
+++ DataInterfaces/Modules/Service/Enumerations/Errors/FiscalPrinterErrorCodes.cs
@@ -15,22 +15,27 @@ namespace ServerService.Exceptions
         /// <summary>
         /// Shift is expired.
         /// </summary>
-        [Localized("FISCAL_PRINTER_SHIFT_EXPIRED")]
+        [Localized("FISCAL_PRINTER_ERROR_SHIFT_EXPIRED")]
         ShiftExpired =1,
         /// <summary>
         /// Shift is closed.
         /// </summary>
-        [Localized("FISCAL_PRINTER_SHIFT_CLOSED")]
+        [Localized("FISCAL_PRINTER_ERROR_SHIFT_CLOSED")]
         ShiftClosed =2,
         /// <summary>
         /// Fiscal printer driver error.
         /// </summary>
-        [Localized("FISCAL_PRINTER_DRIVER_ERROR")]
+        [Localized("FISCAL_PRINTER_ERROR_DRIVER_ERROR")]
         DriverError =3,
         /// <summary>
         /// Fiscal printer error.
         /// </summary>
-        [Localized("FISCAL_PRINTER_ERROR")]
+        [Localized("FISCAL_PRINTER_ERROR_PRINTER_ERROR")]
         PrinterError =4,
+        /// <summary>
+        /// Failed to generate a receipt.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_RECEIPT_GENERATE_ERROR")]
+        ReceiptGenerateError =5,
     }
 }
