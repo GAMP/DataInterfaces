b7ef5ddc7bd5984c2a2536ca049d1995dcd9a613
Oleg Savelos <olegsavelos@gmail.com>
2022-04-15 15:40:39 +0300

    Error code update.

--- DataInterfaces/Modules/Service/Enumerations/Errors/FiscalPrinterErrorCodes.cs
+++ DataInterfaces/Modules/Service/Enumerations/Errors/FiscalPrinterErrorCodes.cs
@@ -11,31 +11,51 @@ namespace ServerService.Exceptions
         /// Fiscal printer is not connected.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_NOT_CONNECTED")]
-        NotConnected=0,
+        NotConnected = 0,
         /// <summary>
         /// Shift is expired.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_SHIFT_EXPIRED")]
-        ShiftExpired =1,
+        ShiftExpired = 1,
         /// <summary>
         /// Shift is closed.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_SHIFT_CLOSED")]
-        ShiftClosed =2,
+        ShiftClosed = 2,
         /// <summary>
         /// Fiscal printer driver error.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_DRIVER_ERROR")]
-        DriverError =3,
+        DriverError = 3,
         /// <summary>
         /// Fiscal printer error.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_PRINTER_ERROR")]
-        PrinterError =4,
+        PrinterError = 4,
         /// <summary>
         /// Failed to generate a receipt.
         /// </summary>
         [Localized("FISCAL_PRINTER_ERROR_RECEIPT_GENERATE_ERROR")]
-        ReceiptGenerateError =5,
+        ReceiptGenerateError = 5,
+        /// <summary>
+        /// No itinital sale receipt was printed.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_NO_RECEIPT_ERROR")]
+        NoReceipt = 6,
+        /// <summary>
+        /// Return receipt can't be generated since there are multiple tax systems.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_MULTIPLE_TAX_SYSTEMS")]
+        MultipleTaxSystems = 7,
+        /// <summary>
+        /// Return receipt can't be generated since there are mixed deposits with other payment methods.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_DEPOSIT_MIXED")]
+        DepositsMixedWithOtherPayments = 8,
+        /// <summary>
+        /// Return receipt can't be generated since there is a payment from deposits that doesn't match the amount of services.
+        /// </summary>
+        [Localized("FISCAL_PRINTER_ERROR_DEPOSIT_NOT_MATCH_SERVICES")]
+        DepositsNotMatchServices = 9,
     }
 }
