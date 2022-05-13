dbdbbae3406aa52437be566164f6be3bbff7d260
klapsis-netprojects <klapsis@netprojects.gr>
2022-05-13 13:45:17 +0300

    Add user lock option in client ui.

---
+++ DataInterfaces/Modules/Client/EventArgs/UserLockChangeEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Client
+{
+    /// <summary>
+    /// Client user lock change args.
+    /// </summary>
+    public class UserLockChangeEventArgs : EventArgs
+    {
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public UserLockChangeEventArgs()
+        { }
+
+        #endregion
+
+        public bool IsLocked { get; set; }
+    }
+}
