690b7eafce7f29614e31e7833826e8d0257710be
Oleg Savelos <olegsavelos@gmail.com>
2025-04-16 17:46:24 +0300

    allign.

--- DataInterfaces/Libraries/SharedLib/Commands/CommandStates.cs
+++ DataInterfaces/Libraries/SharedLib/Commands/CommandStates.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharedLib.Commands
 {
@@ -20,44 +16,44 @@ namespace SharedLib.Commands
         /// </remarks>
         /// </summary>
         None = 0,
-        
+
         /// <summary>
         /// Command is not supported.
         /// </summary>
         NotSupported = 32,
-        
+
         /// <summary>
         /// Invalid command parameters specified.
         /// <remarks>
         /// </remarks>
         /// </summary>
         InvalidParameters = 64,
-        
+
         /// <summary>
         /// State should be set when request or response sending failed.
         /// </summary>
         SendFailed = 128,
-        
+
         /// <summary>
-        /// Occours when command carries an operation or a state update and requested command is not present.
+        /// Occurs when command carries an operation or a state update and requested command is not present.
         /// </summary>
         RequestNotFound = 256,
-        
+
         /// <summary>
         /// This state is set when operation exception is unhandled.
         /// </summary>
         UnhandledOperationException = 512,
-        
+
         /// <summary>
-        /// Occours when command data is not recognized.
+        /// Occurs when command data is not recognized.
         /// </summary>
-        Unrecongnized = 1024,
-        
+        Unrecognized = 1024,
+
         /// <summary>
-        /// Command cannot execute due to authorozation.
+        /// Command cannot execute due to authorization.
         /// </summary>
         UnAuthorized = 2048,
-        
+
         /// <summary>
         /// All states flag.
         /// </summary>
