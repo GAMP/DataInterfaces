571347c33b5e2d2ebd761024fb44d467d09c82a8
Oleg Savelos <olegsavelos@gmail.com>
2026-02-19 13:38:30 +0200

    cleanup.

--- DataInterfaces/Libraries/IntegrationLib/Enumerations/LoginResult.cs
+++ DataInterfaces/Libraries/IntegrationLib/Enumerations/LoginResult.cs
@@ -14,113 +14,109 @@ namespace IntegrationLib
         /// </summary>
         [Localized("LOGIN_RESULT_SUCESS")]
         Success = 0,
+
         /// <summary>
         /// Invalid parameters specified.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_PARAMETERS")]
         InvalidParameters = 1,
+
         /// <summary>
         /// Account is disabled.
         /// </summary>
         [Localized("LOGIN_RESULT_ACCOUNT_DISABLED")]
         AccountDisabled = 2,
+
         /// <summary>
         /// Username is invalid.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_USERNAME")]
         InvalidUserName = 4,
+
         /// <summary>
         /// Password is invalid.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_PASSWORD")]
         InvalidPassword = 8,
+
         /// <summary>
         /// Generic internal error.
         /// </summary>
         [Localized("LOGIN_RESULT_FAILED")]
         Failed = 16,
+
         /// <summary>
         /// Login was denied.
         /// </summary>
         [Localized("LOGIN_RESULT_DENIED")]
         Denied = 32,
-        /// <summary>
-        /// Function timed out.
-        /// </summary>
-        [Obsolete()]
-        [Localized("LOGIN_RESULT_TIMED_OUT")]
-        TimedOut = 64,
-        /// <summary>
-        /// Login function cannot be executed.
-        /// </summary>
-        [Obsolete()]
-        [Localized("LOGIN_RESULT_CANT_EXECUTE")]
-        CantExecute = 128,
+
         /// <summary>
         /// User already logged in.
         /// </summary>
         [Localized("LOGIN_RESULT_ALREADY_LOGGED_IN")]
         AlreadyLoggedIn = 256,
-        /// <summary>
-        /// Login in progress.
-        /// </summary>
-        [Localized("LOGIN_RESULT_IN_PROGRESS")]
-        LoginInProgress = 512,
-        /// <summary>
-        /// Logout in progress.
-        /// </summary>
-        [Localized("LOGIN_RESULT_IN_PROGRESS")]
-        LogoutInProgress = 1024,
+
         /// <summary>
         /// Invalid user id.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_USER")]
         InvalidUserId = 2048,
+
         /// <summary>
         /// Invalid host id.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_HOST")]
         InvalidHostId = 4096,
+
         /// <summary>
         /// Maximum sessions reached.
         /// </summary>
         [Localized("LOGIN_RESULT_MAX_SESSIONS_REACHED")]
         MaximumSessionsReached = 8192,
+
         /// <summary>
         /// Insufficient balance.
         /// </summary>
         [Localized("LOGIN_RESULT_INSUFFICIENT_BALANCE")]
         InsufficientBalance = 16384,
+
         /// <summary>
         /// Invalid user group was specified for login.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_USER_GROUP")]
         InvalidUserGroupId = 32768,
+
         /// <summary>
         /// Slot in use.
         /// </summary>
         [Localized("LOGIN_RESULT_SLOT_IN_USE")]
         SlotInUse = 65536,
+
         /// <summary>
         /// Slot invalid.
         /// </summary>
         [Localized("LOGIN_RESULT_SLOT_INVALID")]
         SlotInvalid = 131072,
+
         /// <summary>
         /// Not in waiting line.
         /// </summary>
         [Localized("LOGIN_RESULT_NOT_IN_WAITING_LINE")]
         NotInWaitingLine = 262144,
+
         /// <summary>
         /// Credentials pair is invalid.
         /// </summary>
         [Localized("LOGIN_RESULT_INVALID_CREDENTIALS")]
         InvalidCredentials = InvalidPassword | InvalidUserName,
+
         /// <summary>
         /// User group use not allowed for the operator.
         /// </summary>
         [Localized("LOGIN_RESULT_OPERATOR_USER_GROUP_DENIED")]
         OperatorUserGroupDenied = 524288,
+
         /// <summary>
         /// Login is restricted by age-hour restrictions.
         /// </summary>
--- DataInterfaces/Modules/Service/Interfaces/Internal/IGizmoService.cs
+++ DataInterfaces/Modules/Service/Interfaces/Internal/IGizmoService.cs
@@ -1,41 +1,61 @@
-﻿using Gizmo.DAL;
-using NetLib;
+﻿using System;
+using System.Collections.Generic;
+using Gizmo.DAL;
 using SharedLib;
-using System;
 
 namespace ServerService
 {
     /// <summary>
     /// Gizmo Service Interface.
     /// </summary>
-    public interface IGizmoService :
-        IHostService,
-        IGizmoDbContextProvider,
-        IUserService
+    public interface IGizmoService : IGizmoDbContextProvider
     {
         /// <summary>
         /// Schedules a user balance event.
         /// This event is used to notify modules of user balance change.
         /// </summary>
         /// <param name="userId">User id.</param>
-        void ScheduleUserBalanceEvent(int userId); 
+        void ScheduleUserBalanceEvent(int userId);
 
         /// <summary>
-        /// Occurs on server startup.
-        /// <remarks>
-        /// This event is called once service is fully initialized.
-        /// </remarks>
+        /// Occurs on one or more host properties change.
         /// </summary>
-        event EventHandler<StartUpEventArgs> Startup;
+        event EventHandler<HostPropertiesChangedEventArgs> HostPropertiesChange;
 
         /// <summary>
-        /// Occurs on service shutdown.
+        /// Occurs on host connection successfully processed.
         /// </summary>
-        event EventHandler<ShutDownEventArgs> Shutdown;
+        event EventHandler<HostIdEventArgs> HostEvent;
 
         /// <summary>
-        /// Occurs on one of network connection change.
+        /// Gets specified property for host.
         /// </summary>
-        event EventHandler<ConnectionChangedArgs> ConnectionChanged;      
+        /// <param name="hostId">Host id.</param>
+        /// <param name="type">Property type.</param>
+        /// <returns>Property value.</returns>
+        object HostGetProperty(int hostId, HostPropertyType type);
+
+        /// <summary>
+        /// Gets properties for host.
+        /// </summary>
+        /// <param name="hostId">Host id.</param>
+        /// <param name="types">Property types.</param>
+        /// <returns>Properties dictionary.</returns>
+        Dictionary<HostPropertyType, object> HostGetProperties(int hostId, HashSet<HostPropertyType> types);
+
+        /// <summary>
+        /// Gets properties for host.
+        /// </summary>
+        /// <param name="hostId">Host id.</param>
+        /// <returns>Properties dictionary.</returns>
+        Dictionary<HostPropertyType, object> HostGetProperties(int hostId);
+
+        /// <summary>
+        /// Gets host ids where property equals to specified value.
+        /// </summary>
+        /// <param name="type">Property type.</param>
+        /// <param name="value">Property value.</param>
+        /// <returns>Host id set.</returns>
+        ISet<int> HostGetWherePropertyEquals(HostPropertyType type, object value);
     }      
 }
